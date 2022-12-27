e1cd660dbe119d72d19e03c36de0b50883de61a7
Mickael Stanislas <contact@mickael-stanislas.com>
2022-12-27 12:16:52 +0100

    feat(dcs): add example flexibleengine_dcs_instance_v1

--- config/dcs/config.go
+++ config/dcs/config.go
@@ -8,15 +8,4 @@ import (
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
 
-	// flexibleengine_dcs_instance_v1
-	// https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/resources/dcs_instance_v1
-	p.AddResourceConfigurator("flexibleengine_dcs_instance_v1", func(r *config.Resource) {
-
-		// password
-		// TODO Secure password
-
-		r.UseAsync = true
-
-	})
-
 }
